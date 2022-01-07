e4c87f79cfd5495879b430ab6a9d6497e17c9699
Aryan Patel <patelaryan1726@gmail.com>
2022-01-07 12:47:48 +0530

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,38 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
     uint256 public totalSupply;
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
 
     // Constructor
-    constructor() public {
-        totalSupply = 1000000;
+    constructor(uint256 _initialSupply) public {
+        // alocate initial supply
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
-    // Set the total number of tokens
-    // Read total Number of tokens
+    // Transfer Function
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // Checking if sender has enough balance to do transaction
+        require(balanceOf[msg.sender] >= _value);
+
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Transfer Event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
